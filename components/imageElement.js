d87ba1c925dc7f316b1a872d668baf7a404f214f
Anthony <anthonyjquattrocchi@gmail.com>
2019-01-05 12:24:42 -0800

    Image Component created. Images added.

---
+++ components/imageElement.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Image} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+        <Image source={this.props.imgsource} style={styles.image} />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  image: {
+      flex: 1,
+      width: null,
+      alignSelf: 'stretch',
+  }
+});
